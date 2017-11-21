9b5c87848a2aefeeb8a3d0a590bf4ef29d341052
ActuallySarah <you@example.com>
2017-11-20 22:58:51 -0600

    its gnu+linux

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+from quotes import quotiebois
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def html_please():
+    stallquote = quotiebois[random.randint(0,5)]
+    return render_template('index.html', quote = stallquote )
---
+++ quotes.py
@@ -0,0 +1,8 @@
+quotiebois = [
+    'What they had in common was mainly love of excellence and programming.',
+    'GNU is not in the public domain. Everyone will be permitted to modify and redistribute GNU, but no distributor will be allowed to restrict its further redistribution. That is to say, proprietary modifications will not be allowed. I want to make sure that all versions of GNU remain free.',
+    'Once GNU is written, everyone will be able to obtain good system software free, just like air.',
+    'There is nothing wrong with wanting pay for work, or seeking to maximize ones income, as long as one does not use means that are destructive. But the means customary in the field of software today are based on destruction.',
+    'A hacker is someone who enjoys playful cleverness, not necessarily with computers. ',
+    'If you focus your mind on the freedom and community that you can build by staying firm, you will find the strength to do it. '
+]
